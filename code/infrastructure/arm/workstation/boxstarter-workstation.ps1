0e9cbd0f550e3c39e551e8cdab1f23260bfc591b
Quisitive_DnA <richard.sidebottom@quisitive.com>
2022-11-08 11:49:15 -0700

    Update boxstarter-workstation.ps1

--- code/infrastructure/arm/workstation/boxstarter-workstation.ps1
+++ code/infrastructure/arm/workstation/boxstarter-workstation.ps1
@@ -82,14 +82,14 @@ try {
   ######################################################
   # Taskbar icons
   ######################################################
-  Write-Host "Adding Icons to the TaskBar"
-  Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
-  Install-ChocolateyPinnedTaskBarItem "%windir%\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe"
-  Install-ChocolateyPinnedTaskBarItem "C:\Windows\explorer.exe"
-  Install-ChocolateyPinnedTaskBarItem "%windir%\system32\cmd.exe"
-  Install-ChocolateyPinnedTaskBarItem "C:\Program Files\Google\Chrome\Application\chrome.exe"
+  # Write-Host "Adding Icons to the TaskBar"
+  # Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
+  # Install-ChocolateyPinnedTaskBarItem "%windir%\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe"
+  # Install-ChocolateyPinnedTaskBarItem "C:\Windows\explorer.exe"
+  # Install-ChocolateyPinnedTaskBarItem "%windir%\system32\cmd.exe"
+  # Install-ChocolateyPinnedTaskBarItem "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
-  %windir%\system32\cmd.exe
+  # %windir%\system32\cmd.exe
 
   ######################################################
   # Add to the path
