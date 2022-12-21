89729ec06664db2725740142df48f7308e0ac401
Richard Sidebottom <richard.sidebottom@quisitive.com>
2022-12-21 14:54:57 -0700

    removed dev-vm and updated boxstarter script

---
+++ code/infrastructure/arm/workstation/scripts/install-taskbar.ps1
@@ -0,0 +1,6 @@
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Windows\explorer.exe"
+Install-ChocolateyPinnedTaskBarItem "%windir%\system32\cmd.exe"
+Install-ChocolateyPinnedTaskBarItem "C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+%windir%\system32\cmd.exe
